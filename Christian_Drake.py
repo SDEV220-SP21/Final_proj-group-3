f6122f5cab05503c26383e6fae611ff54af1781b
Christian Drake <39897643+NuggetKitten@users.noreply.github.com>
2021-05-10 18:58:38 -0400

    Add files via upload

---
+++ Christian_Drake.py
@@ -0,0 +1,19 @@
+#random word choice
+#def RandomWord
+#import file as reading
+#check line amount on file
+# grab line (randomint 0 - lines - 1)
+# check length of line
+#turn word into list for later use
+
+
+#player input / misscheck
+#get input
+#check if not number
+#if number repeat else:
+# check if in Chosen_word
+#  words  = list(string)
+# while count < wordlength
+# if input == words[count]
+#  correct = 1,  reveal = count
+#if correct = 0, miss += 1
